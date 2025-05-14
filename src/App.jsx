f6c22b82a55d8db28bf6f9e6915839a5d811ae43
Filipe Martins Pinto <filipemartinsep@gmail.com>
2025-05-14 16:30:24 -0300

    feat: add ProductCategoryRow component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import "./App.css";
 
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">{category}</th>
+    </tr>
+  );
+}
+
 function App() {
   return <p>Hello, world!</p>;
 }
